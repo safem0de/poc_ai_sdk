3067de1f4d7699f5a98d62ed9988ab253b85d025
Naweeparb <naweeparbw@borneothai.com>
2025-07-31 14:03:48 +0700

    add azure openai

---
+++ 10_6_azure-openai.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import { AzureOpenAI } from "openai";
+
+const endpoint = process.env.AZURE_ENDPOINT!;
+const modelName = process.env.AZURE_MODEL_NAME!;
+const deployment = process.env.AZURE_DEPLOYMENT_NAME!;
+
+export async function main() {
+
+  const apiKey = process.env.AZURE_API_KEY!;
+  const apiVersion = process.env.API_VERSION!;
+  const options = { endpoint, apiKey, deployment, apiVersion }
+
+  const client = new AzureOpenAI(options);
+
+  const response = await client.chat.completions.create({
+    messages: [
+      { role:"system", content: "You are a helpful assistant." },
+      { role:"user", content: "I am going to Paris, what should I see?" }
+    ],
+    max_tokens: 2048,
+      temperature: 1,
+      top_p: 1,
+      model: modelName
+  });
+
+  console.log(response.choices[0].message.content);
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
